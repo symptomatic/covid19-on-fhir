bb1b64c5d5227ae46639ba35936db6fb327fe9da
Abigail Watson <abigailwatson@JurrassControl3.attlocal.net>
2023-03-12 14:36:50 -0500

    Gravity PatientApp testing

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'symptomatic:covid19-on-fhir',
-    version: '0.5.1',
+    version: '0.5.2',
     summary: 'COVID19 Hackathon',
     git: 'https://github.com/symptomatic/covid19-hackathon',
     documentation: 'README.md'
